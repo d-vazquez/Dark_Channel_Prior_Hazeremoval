acb6e515e0590c31ccf986d79dee991684abf939
Dario Vazquez <dario_vazquez@apple.com>
2023-03-08 22:46:14 -0800

    Changed to byte in opencv

--- DCP_opencv/main.cpp
+++ DCP_opencv/main.cpp
@@ -46,15 +46,9 @@ const Mat DarkChannel(Mat &img, int sz)
     // 'erode' image, so calculate the minimun value in the window given by sz
     Mat kernel = getStructuringElement(cv::MorphShapes::MORPH_RECT, Size(sz,sz));
     
-    cout << "kernel= " << endl << kernel << endl;
-    
-    Mat dark = Mat::zeros(img.rows, img.cols, CV_32FC3);
+    Mat dark;
     cv::erode(dc, dark, kernel);
-    
-//    imshow("img", img);
-//    imshow("dark", dark);
-//    waitKey(0);
-    
+ 
     return dark;
 }
 
@@ -77,9 +71,9 @@ const Scalar AtmLight(Mat &im, Mat &dark)
     Scalar atmsum(0, 0, 0, 0);
     for(int ind = 0; ind < numpx; ind++)
     {
-        atmsum.val[0] += imvec.at<Vec3f>(0, indices.at<int>(0,ind))[0];
-        atmsum.val[1] += imvec.at<Vec3f>(0, indices.at<int>(0,ind))[1];
-        atmsum.val[2] += imvec.at<Vec3f>(0, indices.at<int>(0,ind))[2];
+        atmsum.val[0] += imvec.at<Vec3b>(0, indices.at<int>(0,ind))[0];
+        atmsum.val[1] += imvec.at<Vec3b>(0, indices.at<int>(0,ind))[1];
+        atmsum.val[2] += imvec.at<Vec3b>(0, indices.at<int>(0,ind))[2];
     }
     
     atmsum.val[0] = atmsum.val[0]/ numpx;
@@ -101,13 +95,14 @@ const Mat  TransmissionEstimate(Mat &im, Scalar A, int sz)
     cv::split(im, img3_ch);
     cv::split(im, im_ch);
     
-    img3_ch[0] = im_ch[0] / A.val[0];
-    img3_ch[1] = im_ch[1] / A.val[1];
-    img3_ch[2] = im_ch[2] / A.val[2];
+    img3_ch[0] = (im_ch[0] / A.val[0]) * 255;
+    img3_ch[1] = (im_ch[1] / A.val[1]) * 255;
+    img3_ch[2] = (im_ch[2] / A.val[2]) * 255;
     
     cv::merge(img3_ch, im3);
     
-    Mat transmission = 1 - omega*DarkChannel(im3,sz);
+    Mat transmission = 255 - omega*DarkChannel(im3,sz);
+    
     
     return transmission;
 }
@@ -117,20 +112,35 @@ const Mat TransmissionRefine(Mat &im, Mat &et)
 {
     Mat gray;
     cvtColor(im, gray, cv::COLOR_BGR2GRAY);
-    return Guidedfilter(gray, et, 60, 0.0001);
+    return Guidedfilter(gray, et, 60, 1);
 }
 
 const Mat Guidedfilter(Mat &im, Mat &p, int r, int eps)
 {
+    cout << im.type() << endl;
+    
+    cout << "im before = " << endl << im(Range(0,5),Range(0,5)) << endl;
+    
+    im.convertTo(im, CV_32FC3);
+    cv::normalize(im, im, 0, 1, cv::NORM_MINMAX);
+    
+    p.convertTo(p, CV_32FC1);
+    cv::normalize(p, p, 0, 1, cv::NORM_MINMAX);
+    
+    
+    cout << "im after = " << endl << im(Range(0,5),Range(0,5)) << endl;
+    
+    cout << im.type() << endl;
+    
     Mat q, mean_I, mean_p, mean_Ip;
     Mat mean_II, mean_a, mean_b;
     Mat im_p;
     
     cv::boxFilter(im, mean_I, CV_32F, Size(r,r));
     
-    imshow("im", im);
-    imshow("mean_I", mean_I);
-    waitKey(0);
+//    imshow("im", im);
+//    imshow("mean_I", mean_I);
+//    waitKey(0);
     
     cv::boxFilter(p, mean_p, CV_32F, Size(r,r));
     
@@ -148,26 +158,67 @@ const Mat Guidedfilter(Mat &im, Mat &p, int r, int eps)
     cv::boxFilter(b, mean_b, CV_32F, Size(r,r));
     
     q = im.mul(mean_a) + mean_b;
+    
+    
+    
+    cout << "q before = " << endl << q(Range(0,5),Range(0,5)) << endl;
+    
+    cv::normalize(q, q, 0, 255, cv::NORM_MINMAX);
+
+    cout << "q normalize = " << endl << q(Range(0,5),Range(0,5)) << endl;
+
+    
+    q.convertTo(q, CV_8UC1);
+    
+    
+    cout << "q after = " << endl << q(Range(0,5),Range(0,5)) << endl;
+    
+    cout << im.type() << endl;
+    
     return q;
 }
 
 
 const Mat Recover(Mat &im, Mat &t, Scalar A, float tx=0.1)
 {
+    cout << im.type() << endl;
+    cout << t.type() << endl;
+//
+//    Mat res;
+//    
+//    t = cv::max(t, tx); // Make sure t does not contain 0
+//    
+//    // perform operation on each channel
+//    vector<Mat> res_ch(3);
+//    vector<Mat> im_ch(3);
+//    cv::split(im, im_ch);
+//    
+//    res_ch[0] = (im_ch[0] - A.val[0])/t + A.val[0];
+//    res_ch[1] = (im_ch[1] - A.val[1])/t + A.val[1];
+//    res_ch[2] = (im_ch[2] - A.val[2])/t + A.val[2];
+//    
+//    cv::merge(res_ch, res);
+    
+    cout << "im = " << endl << im(Range(0,5),Range(0,5)) << endl;
+    cout << "t = " << endl << t(Range(0,5),Range(0,5)) << endl;
+    cout << "A = " << endl << A.val[0] << endl;
+    
     Mat res;
+    res.create(im.rows, im.cols, im.type());
     
-    t = cv::max(t, tx); // Make sure t does not contain 0
+    for(int _row = 0; _row < res.rows; _row++)
+    {
+        for(int _col = 0; _col < res.cols; _col++)
+        {
+            float factor = 255.f/t.at<uchar>(_row, _col);
+            
+            res.at<Vec3b>(_row, _col)[0] = (im.at<Vec3b>(_row, _col)[0] - A.val[0])*factor + A.val[0];
+            res.at<Vec3b>(_row, _col)[1] = (im.at<Vec3b>(_row, _col)[1] - A.val[1])*factor + A.val[1];
+            res.at<Vec3b>(_row, _col)[2] = (im.at<Vec3b>(_row, _col)[2] - A.val[2])*factor + A.val[2];
+            
+        }
+    }
     
-    // perform operation on each channel
-    vector<Mat> res_ch(3);
-    vector<Mat> im_ch(3);
-    cv::split(im, im_ch);
-    
-    res_ch[0] = (im_ch[0] - A.val[0])/t + A.val[0];
-    res_ch[1] = (im_ch[1] - A.val[1])/t + A.val[1];
-    res_ch[2] = (im_ch[2] - A.val[2])/t + A.val[2];
-    
-    cv::merge(res_ch, res);
     
     return res;
 }
@@ -193,12 +244,15 @@ int main(int argc, const char ** argv)
     string filename = parser.get<string>(0);
     
     Mat I = imread(filename, IMREAD_COLOR);
-    I.convertTo(I, CV_32FC3);
-    cv::normalize(I, I, 0, 1, cv::NORM_MINMAX);
+//    I.convertTo(I, CV_32FC3);
+//    cv::normalize(I, I, 0, 1, cv::NORM_MINMAX);
     
     auto start = high_resolution_clock::now();
     
     Mat dark    = DarkChannel(I,15);
+//    cout << "dark = " << endl << dark(Range(0,5),Range(0,5)) << endl;
+//    imshow("dark", dark);
+//    waitKey(0);
     
     auto _darkchannel = high_resolution_clock::now();
     
@@ -207,10 +261,16 @@ int main(int argc, const char ** argv)
     auto _airlight = high_resolution_clock::now();
     
     Mat te      = TransmissionEstimate(I,A,15);
+//    cout << "te = " << endl << te(Range(0,5),Range(0,5)) << endl;
+//    imshow("te", te);
+//    waitKey(0);
     
     auto _transmision = high_resolution_clock::now();
     
     Mat t       = TransmissionRefine(I,te);
+//    cout << "t = " << endl << t(Range(0,5),Range(0,5)) << endl;
+//    imshow("t", t);
+//    waitKey(0);
     
     auto _transmision_refine = high_resolution_clock::now();
     
